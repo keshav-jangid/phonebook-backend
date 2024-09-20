6d03b900b7539cbf8e7a0e5e56ea68849c0bacf0
keshvcode <info.keshavjangid@gmail.com>
2024-09-20 18:12:24 +0530

    initial debug

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`the server is runnig on port number ${PORT}`);
 });
